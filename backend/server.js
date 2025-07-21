31c8c230aa31b110e1b01c5a326a57216d3f4213
01akhil <bhadula245@gmail.com>
2025-07-21 23:48:44 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 
+app.use(cors({
+  origin: 'https://assignment-blond-xi.vercel.app',
+  credentials: true // if you're using cookies or auth headers
+}));
 
 
 app.use(express.json({ limit: '50mb' }));
